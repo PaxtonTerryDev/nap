f4f10beb92c8e77e6aa5e76474cec286ce7c7c1b
Paxton Terry <paxtonterrydev@gmail.com>
2026-01-21 23:35:20 -0700

    Basic command class definition

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
-console.log("Hello, World!");
+interface CommandProps {
+  cmd: string;
+}
+
+interface Command {
+  cmd: string;
+  flags: Flag[];
+}
+
+const command: Command = {
+  cmd: "init",
+  flags: [
+    {
+      name: "path",
+      type: "string",
+      description: "The path to the directory it should initialize to",
+    },
+    {
+      name: "yes",
+      type: "string",
+      description: "Skip initialization questions, and prefer defaults",
+    }
+  ]
+}
+
